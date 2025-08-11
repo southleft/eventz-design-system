3d2efd7cbf303566d27c86ea97a3c03cfd480308
Tony M <tony@fbpllc.com>
2025-08-11 08:03:48 -0700

    add TypeScript, Prettier, and ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier' // disables conflicting rules
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+  rules: {
+    'unused-imports/no-unused-imports': 'warn',
+    'import/order': ['warn', { 'newlines-between': 'always' }]
+  }
+};
