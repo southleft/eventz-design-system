15b110f85edbbc4349df669f966333cf36762eb8
Tony M <tony@fbpllc.com>
2025-08-14 09:19:52 -0700

    add a test component for local development

---
+++ src/components/TestDialog/TestDialog.stories.tsx
@@ -0,0 +1,9 @@
+// src/components/TestDialog.stories.tsx
+import { TestDialog } from './TestDialog';
+
+export default {
+  title: 'Components/TestDialog',
+  component: TestDialog
+};
+
+export const Default = () => <TestDialog />;
