d845a490718177e7b9889e895e2cb11cc122b0e3
Tony M <tony@fbpllc.com>
2025-08-15 08:37:46 -0700

    add test tokens

---
+++ styles/tokens/types.ts
@@ -0,0 +1,15 @@
+// styles/tokens/types.ts
+
+export type CoreColorKey =
+  | 'blue.50'
+  | 'blue.500'
+  | 'blue.900'
+  | 'gray.50'
+  | 'gray.500'
+  | 'gray.900';
+
+export type ThemeColorKey = 'primary' | 'background' | 'foreground';
+
+export type TypographyKey = 'body' | 'heading';
+
+export type DialogTokenKey = 'background' | 'text' | 'titleSize';
