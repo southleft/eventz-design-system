b1186737c149d3e692a2e69d619951ae327e8d37
Tony M <tony@fbpllc.com>
2025-09-09 13:25:54 -0700

    remove test components

--- packages/blueprints/src/components/TestDialog/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './TestDialog.contract';
-export * from './TestDialog.styleMap';
--- packages/core/src/components/TestDialog/TestDialog.stories.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { TestDialog } from './TestDialog';
-
-export default {
-  title: 'Components/TestDialog',
-  component: TestDialog
-};
-
-export const Default = () => <TestDialog />;
