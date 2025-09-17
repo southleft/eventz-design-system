cb56ae6aace3e75afe36eab37fe96ee8e2e098e1
Tony M <tony@fbpllc.com>
2025-09-16 17:14:41 -0700

    refactor IconButton classes as template literals

--- packages/core/src/components/IconButton/IconButton.tsx
+++ packages/core/src/components/IconButton/IconButton.tsx
@@ -23,58 +23,65 @@ export interface IconButtonProps
     >,
     IconButtonOwnProps {}
 
-const baseClasses = [
-  'inline-flex select-none items-center justify-center',
-  'outline-none',
-  'transition-colors',
-  'focus-visible:ring-2',
-  'focus-visible:ring-comp-icon-button-focus-color-ring',
-  'focus-visible:ring-offset-2',
-  'disabled:opacity-50 disabled:pointer-events-none',
-  'whitespace-nowrap'
-] as const;
+const baseClasses = `
+  inline-flex select-none items-center justify-center
+  outline-none
+  transition-colors
+  focus-visible:ring-2
+  focus-visible:ring-comp-icon-button-focus-color-ring
+  focus-visible:ring-offset-2
+  disabled:opacity-50 disabled:pointer-events-none
+  whitespace-nowrap
+`;
 
-const containerClasses = ['h-10', 'w-10', 'rounded-md'] as const;
+const containerClasses = `
+  h-10
+  w-10
+  rounded-md
+`;
 
-const variantClasses: Record<Variant, readonly string[]> = {
-  primary: [
-    'bg-comp-icon-button-primary-color-background-default',
-    'text-comp-icon-button-primary-color-foreground-default',
-    'hover:bg-comp-icon-button-primary-color-background-hover',
-    'active:bg-comp-icon-button-primary-color-background-active'
-  ],
-  secondary: [
-    'bg-comp-icon-button-secondary-color-background-default',
-    'text-comp-icon-button-secondary-color-foreground-default',
-    'hover:bg-comp-icon-button-secondary-color-background-hover',
-    'active:bg-comp-icon-button-secondary-color-background-active'
-  ],
-  bare: [
-    'bg-transparent',
-    'text-comp-icon-button-bare-color-foreground-default',
-    'hover:bg-comp-icon-button-bare-color-background-hover',
-    'active:bg-comp-icon-button-bare-color-background-active'
-  ],
-  knockout: [
-    'bg-transparent',
-    'text-comp-icon-button-knockout-color-foreground-default',
-    'border',
-    'border-comp-icon-button-knockout-color-border-default',
-    'hover:bg-comp-icon-button-knockout-color-background-hover',
-    'active:bg-comp-icon-button-knockout-color-background-active'
-  ],
-  bareKnockout: [
-    'bg-transparent',
-    'text-comp-icon-button-bare-knockout-color-foreground-default',
-    'border',
-    'border-comp-icon-button-bare-knockout-color-border-default',
-    'hover:bg-comp-icon-button-bare-knockout-color-background-hover',
-    'active:bg-comp-icon-button-bare-knockout-color-background-active'
-  ]
+const variantClasses: Record<Variant, string> = {
+  primary: `
+    bg-comp-icon-button-primary-color-background-default
+    text-comp-icon-button-primary-color-foreground-default
+    hover:bg-comp-icon-button-primary-color-background-hover
+    active:bg-comp-icon-button-primary-color-background-active
+  `,
+  secondary: `
+    bg-comp-icon-button-secondary-color-background-default
+    text-comp-icon-button-secondary-color-foreground-default
+    hover:bg-comp-icon-button-secondary-color-background-hover
+    active:bg-comp-icon-button-secondary-color-background-active
+  `,
+  bare: `
+    bg-transparent
+    text-comp-icon-button-bare-color-foreground-default
+    hover:bg-comp-icon-button-bare-color-background-hover
+    active:bg-comp-icon-button-bare-color-background-active
+  `,
+  knockout: `
+    bg-transparent
+    text-comp-icon-button-knockout-color-foreground-default
+    border
+    border-comp-icon-button-knockout-color-border-default
+    hover:bg-comp-icon-button-knockout-color-background-hover
+    active:bg-comp-icon-button-knockout-color-background-active
+  `,
+  bareKnockout: `
+    bg-transparent
+    text-comp-icon-button-bare-knockout-color-foreground-default
+    border
+    border-comp-icon-button-bare-knockout-color-border-default
+    hover:bg-comp-icon-button-bare-knockout-color-background-hover
+    active:bg-comp-icon-button-bare-knockout-color-background-active
+  `
 };
 
 const stateClasses = {
-  loading: ['cursor-wait', 'data-[loading=true]:opacity-100'] as const
+  loading: `
+    cursor-wait
+    data-[loading=true]:opacity-100
+  `
 } as const;
 
 const slotClasses = {
