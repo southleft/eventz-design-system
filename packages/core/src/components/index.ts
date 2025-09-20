da084d3c9222f4ccd04c54e047e8d436873fe64a
Tony M <tony@fbpllc.com>
2025-09-20 10:30:49 -0700

    update barrel files

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -1,5 +1,7 @@
 export * from './Badge';
 export * from './Button';
+export * from './Checkbox';
+export * from './CheckboxGroup';
 export * from './Control';
 export * from './IconButton';
 export * from './TextLink';
