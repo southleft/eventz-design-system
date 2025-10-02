b3e29cda037ea974f0fe47f1e706207c27b3c43d
Tony M <tony@fbpllc.com>
2025-10-01 17:46:41 -0700

    Chromatic build test 13

--- packages/core/.storybook/main.ts
+++ packages/core/.storybook/main.ts
@@ -1,18 +1,17 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: [
-    '../src/**/*.mdx',
-    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
-  ],
-  addons: [
-    '@chromatic-com/storybook',
-    '@storybook/addon-docs',
-    '@storybook/addon-a11y'
-  ],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@chromatic-com/storybook', '@storybook/addon-docs', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/react-vite',
     options: {}
+  },
+  viteFinal(config) {
+    config.plugins = (config.plugins ?? []).filter(
+      plugin => plugin && 'name' in plugin && (plugin as any).name !== 'vite:dts'
+    );
+    return config;
   }
 };
 
