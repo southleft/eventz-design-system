966c481cdfb36fc46b986bff1d62412991856e17
Tony M <tony@fbpllc.com>
2025-11-02 13:55:44 -0800

    add react docgen

--- packages/core/.storybook/main.ts
+++ packages/core/.storybook/main.ts
@@ -11,6 +11,18 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {}
   },
+  typescript: {
+    // Use TS-aware docgen so extended interfaces are included
+    reactDocgen: 'react-docgen-typescript',
+    // Filter out React DOM/HTML attributes coming from @types/react
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: prop => {
+        const parentFile = prop.parent?.fileName ?? '';
+        return !/node_modules\/@types\/react/i.test(parentFile);
+      }
+    }
+  },
   viteFinal(config) {
     config.plugins = (config.plugins ?? []).filter(
       plugin => plugin && 'name' in plugin && (plugin as any).name !== 'vite:dts'
